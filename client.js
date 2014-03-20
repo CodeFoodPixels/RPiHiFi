cbe57c9271fe20d370b2328281a7762d4f7c1f6e
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-03-20 00:16:35 +0000

    Trying to sync the audio

--- client.js
+++ client.js
@@ -1,8 +1,30 @@
 var net = require('net'),
     speakerLib = require('speaker'),
     speaker = new speakerLib(),
-    stream = require('stream').Readable,
-    client = net.connect({port: 8080}, function() {
+    startTime = 0,
+    beforeStart = 0,
+    Transform = require('stream').Transform,
+    stream = new Transform(),
+    client = net.connect({port: 8080, host: "127.0.0.1"}, function() {
 });
 
-client.pipe(speaker);
+stream._transform = function (chunk, enc, next) {
+    this.push(chunk)
+    next();
+}
+
+// client.pipe(speaker);
+var i = 0;
+client.on('data', function(data){
+    var obj = JSON.parse(data.toString('utf8'));
+    // console.log(obj.time);
+    if (startTime === 0) {
+        startTime = obj.time;
+        beforeStart = startTime - (new Date()).getTime();
+        setTimeout(function(){
+            stream.pipe(speaker)
+        }, beforeStart);
+    }
+    console.log(new Buffer(obj.chunk, 'hex'));
+    stream.push(new Buffer(obj.chunk, 'hex'));
+})
--- server.js
+++ server.js
@@ -9,23 +9,42 @@ var net = require('net'),
     throttledStream = "",
     newClients = [],
     Writable = require('stream').Writable,
-    ws = Writable(),
+    Transform = require('stream').Transform,
+    ws = new Writable(),
+    pipeExport = new Transform(),
+    exportedStream = "",
     discard = "";
 
 ws._write = function (chunk, enc, next) {
     next();
 };
 
+pipeExport._transform = function (chunk, enc, next) {
+    this.push(new Buffer('{"chunk": "'+chunk.toString('hex')+'", "time": '+((new Date()).getTime()+3000)+'}'));
+    next();
+}
+
 decoder.on("format", function(data){
     throttle = new Throttle((data.sampleRate*data.bitDepth*data.channels)/8);
     throttledStream = stream.pipe(throttle);
-    discard = throttledStream.pipe(ws);
+    exportedStream = throttledStream.pipe(pipeExport),
+    discard = exportedStream.pipe(ws);
     server = net.createServer(function(socket) {
             socket.pipe(socket);
-            throttledStream.pipe(socket);
-            socket.on('error', function (exc) {
-                console.log("ignoring exception: " + exc);
-            });
+            newClients.push(socket);
         });
     server.listen(8080);
+    setInterval(function(){
+        addNewClients();
+    }, 1000);
 })
+
+function addNewClients(){
+    for (var i = newClients.length; i > 0; i--) {
+        exportedStream.pipe(newClients[i-1]);
+        newClients[i-1].on('error', function (exc) {
+            console.log("ignoring exception: " + exc);
+        });
+        newClients.pop();
+    }
+}
